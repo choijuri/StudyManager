633835b6d553359517e7060f78ab29ecd015ba5f
Jeong-Yoon <yj4025@gmail.com>
2019-03-21 17:36:00 +0900

    fix commit

--- src/test/java/my/examples/StudyManager/CategoryRepositoryTest.java
+++ src/test/java/my/examples/StudyManager/CategoryRepositoryTest.java
@@ -1,14 +1,21 @@
 package my.examples.StudyManager;
 
+import my.examples.StudyManager.domain.Category;
 import my.examples.StudyManager.repository.CategoryRepository;
+import org.hibernate.validator.constraints.URL;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Iterator;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
-@DataJpaTest
+//@DataJpaTest
+@SpringBootTest
 public class CategoryRepositoryTest {
     @Autowired
     CategoryRepository categoryRepository;
