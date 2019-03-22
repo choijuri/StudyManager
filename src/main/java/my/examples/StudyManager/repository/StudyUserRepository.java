1282967bc68b4ac6e7c086c0551c2541a509bcc0
Jeong-Yoon <yj4025@gmail.com>
2019-03-22 11:42:43 +0900

    fix commit

--- src/main/java/my/examples/StudyManager/repository/StudyUserRepository.java
+++ src/main/java/my/examples/StudyManager/repository/StudyUserRepository.java
@@ -1,7 +1,8 @@
 package my.examples.StudyManager.repository;
 
+import my.examples.StudyManager.domain.StudyUser;
 import my.examples.StudyManager.domain.StudyUserId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudyUser extends JpaRepository<StudyUser, StudyUserId> {
+public interface StudyUserRepository extends JpaRepository<StudyUser, StudyUserId> {
 }
