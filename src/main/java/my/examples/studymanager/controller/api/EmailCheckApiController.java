2b6d0e0caad1fcadb24230b5d9369daa9743c2fd
Jeong-Yoon <yj4025@gmail.com>
2019-04-02 12:06:25 +0900

    fix commit

--- src/main/java/my/examples/studymanager/controller/api/EmailCheckApiController.java
+++ src/main/java/my/examples/studymanager/controller/api/EmailCheckApiController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/emailchk")
+@RequestMapping("/api/emailCheck")
 @RequiredArgsConstructor
 public class EmailCheckApiController {
     private final UserService userService;
