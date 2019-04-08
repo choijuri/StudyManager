0632a78a5b3a6f4ca114ccb2abf5270749bcc9d2
Jeong-Yoon <yj4025@gmail.com>
2019-04-08 16:49:41 +0900

    json-lib defendendy 추가, csrf 문제 해결

--- src/main/java/my/examples/studymanager/config/SecurityConfig.java
+++ src/main/java/my/examples/studymanager/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin() // 사용자가 정의하는 로그인 화면을 만들겠다.
                 .loginProcessingUrl("/users/login") // 로그인 화면
-                .loginPage("/users/login") // 사용자가 입력한 id, password가 전달되는 url경로(필터가처리)
+                .loginPage("/users/login").permitAll() // 사용자가 입력한 id, password가 전달되는 url경로(필터가처리)
                 .usernameParameter("id")
                 .passwordParameter("password")
                 .failureUrl("/users/login?fail=true");
