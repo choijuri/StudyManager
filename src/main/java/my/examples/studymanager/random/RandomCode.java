9d128f85dc31443815cb7c118ed1f87bded95e97
Jeong-Yoon <yj4025@gmail.com>
2019-04-22 18:05:05 +0900

    code 생성 시 특수문자 제외

--- src/main/java/my/examples/studymanager/random/RandomCode.java
+++ src/main/java/my/examples/studymanager/random/RandomCode.java
@@ -6,13 +6,19 @@ public class RandomCode {
 
     public String makeRandomCode() {
         Random ran = new Random();
-        String value = "";
+        StringBuffer sb = new StringBuffer();
+        int cnt = 0;
 
-        for (int i = 0; i < 8; i++) {     //  원하는 난수의 길이
-            int num1 = (int) 48 + (int) (ran.nextDouble() * 74);
-            value = value + (char) num1;
+//        for (int i = 0; i < 8; i++) { //  원하는 난수의 길이
+        while (cnt < 8) { // 원하는 난수의 길이
+            int num1 = (int) 48 + (int) (ran.nextDouble() * 74);    //알파벳 대,소문자, 숫자범위의 아스키값발생
+            if ((num1 >= 48 && num1 <= 57) || (num1 >= 65 && num1 <= 90) || (num1 >= 97 && num1 <= 122)) // 특수문자 제외
+            {
+                sb.append((char) num1);    //아스키코드값을 char로 형변환 후 저장
+                cnt++;
+            }
         }
-        return value;
+        return sb.toString();
     }
 }
 
