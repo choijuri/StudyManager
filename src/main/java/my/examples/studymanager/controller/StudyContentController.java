d17ec4785e1a07f42f01005bfd2fff16201b3769
Jeong-Yoon <yj4025@gmail.com>
2019-04-25 19:56:00 +0900

    Content페이지로 curriculumContent, curriculumDetailContent 값 넘기기 성공, 페이지 이동 수정 중

--- src/main/java/my/examples/studymanager/controller/StudyContentController.java
+++ src/main/java/my/examples/studymanager/controller/StudyContentController.java
@@ -2,12 +2,11 @@ package my.examples.studymanager.controller;
 
 import lombok.RequiredArgsConstructor;
 import my.examples.studymanager.domain.StudyContent;
+import my.examples.studymanager.dto.StudyContentDto;
 import my.examples.studymanager.service.StudyContentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,11 +14,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class StudyContentController {
     private final StudyContentService studyContentService;
 
-    @GetMapping
-    public String studyContent(@RequestParam(name = "curriculumDetailId") Long curriculumDetailId, Model model) {
-        StudyContent studyContent = studyContentService.getStudyContentByCurriculumId(curriculumDetailId);
-
+    @PostMapping
+    public String studyContent(@RequestBody StudyContentDto studyContentDto, Model model ) {
+        StudyContent studyContent = studyContentService.getStudyContentByCurriculumId(studyContentDto.getCurriculumDetailId());
         model.addAttribute("studyContent", studyContent);
+        model.addAttribute("curriculumContent", studyContentDto.getCurriculumContent());
+        model.addAttribute("curriculumDetailContent",studyContentDto.getCurriculumDetailContent());
+
         return "study/studyContent";
     }
 }
---
+++ src/main/java/my/examples/studymanager/dto/StudyContentDto.java
@@ -0,0 +1,18 @@
+package my.examples.studymanager.dto;
+
+import lombok.*;
+
+import javax.persistence.Lob;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudyContentDto {
+    private Long curriculumDetailId;
+    @Lob
+    private String curriculumContent;
+    @Lob
+    private String curriculumDetailContent;
+}
