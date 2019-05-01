8f81b0aab4d30cbb064ca83ecf6657e88e9e1024
Jeong-Yoon <yj4025@gmail.com>
2019-05-01 19:25:55 +0900

    study detail 화면에서 curriculumDetail show(), hide() 기능

--- src/main/java/my/examples/studymanager/controller/HomeController.java
+++ src/main/java/my/examples/studymanager/controller/HomeController.java
@@ -1,10 +1,8 @@
 package my.examples.studymanager.controller;
 
 import lombok.RequiredArgsConstructor;
-import my.examples.studymanager.security.StudyManagerSecurityUser;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -15,7 +13,6 @@ public class HomeController {
     @GetMapping("/index")
     public String index( ){
 
-//        System.out.println("================"+SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         if(!SecurityContextHolder.getContext().getAuthentication().getPrincipal().equals("anonymousUser")){
             return "redirect:/study/list";
         }
