8476fd72a00425d8cde18202086920edd8f85943
choijuri <chlwnfl22@gmail.com>
2019-05-09 14:52:43 +0900

    api url수정, return 타입 수정

--- src/main/java/my/examples/studymanager/controller/api/CurriculumApiController.java
+++ src/main/java/my/examples/studymanager/controller/api/CurriculumApiController.java
@@ -4,13 +4,15 @@ package my.examples.studymanager.controller.api;
 import lombok.RequiredArgsConstructor;
 import my.examples.studymanager.dto.CurriculumFormDto;
 import my.examples.studymanager.service.CurriculumService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/curriculum")
+@RequestMapping("/api/curriculums")
 @RequiredArgsConstructor
 public class CurriculumApiController {
     private final CurriculumService curriculumService;
@@ -18,10 +20,10 @@ public class CurriculumApiController {
 
     //커리큘럼 등록
     @PostMapping
-    public List<CurriculumFormDto> addCurriculum(@Valid @RequestBody List<CurriculumFormDto> curriculumFormDtoList){
-
+    public ResponseEntity<Long> addCurriculum(@Valid @RequestBody List<CurriculumFormDto> curriculumFormDtoList){
         curriculumService.addCurriculum(curriculumFormDtoList);
-        return curriculumFormDtoList;
+        Long studyId = curriculumFormDtoList.get(0).getStudyId();
+        return new ResponseEntity<>(studyId, HttpStatus.CREATED);
     }
 
 }
--- src/main/java/my/examples/studymanager/controller/api/MessageApiController.java
+++ src/main/java/my/examples/studymanager/controller/api/MessageApiController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import my.examples.studymanager.dto.SendMessageDto;
 import my.examples.studymanager.security.StudyManagerSecurityUser;
 import my.examples.studymanager.service.MessageService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,23 +13,25 @@ import javax.validation.Valid;
 
 
 @RestController
-@RequestMapping("/api/message")
+@RequestMapping("/api/messages")
 @RequiredArgsConstructor
 public class MessageApiController {
     private final MessageService messageService;
 
     //메시지 보내기
     @PostMapping
-    public SendMessageDto sendMessage(@Valid @RequestBody SendMessageDto sendMessageDto){
+    public ResponseEntity<SendMessageDto> sendMessage(@Valid @RequestBody SendMessageDto sendMessageDto){
         messageService.addMessage(sendMessageDto);
-        return sendMessageDto;
+        return new ResponseEntity<>(sendMessageDto, HttpStatus.CREATED);
     }
+
 
     //메세지 알림 정보 가져오기
     @GetMapping
-    public int getApiMessageCount(){
+    public ResponseEntity<Integer> getApiMessageCount(){
         StudyManagerSecurityUser securityUser = (StudyManagerSecurityUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return messageService.getNewMessageCount(securityUser.getId());
+        int count =  messageService.getNewMessageCount(securityUser.getId());
+        return new ResponseEntity<>(count, HttpStatus.OK);
     }
 
 }
