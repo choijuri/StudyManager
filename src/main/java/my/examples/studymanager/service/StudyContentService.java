3a614bd05c5013490a7c589eb18c490def4fec81
Jeong-Yoon <yj4025@gmail.com>
2019-05-15 15:14:54 +0900

    fix commit

--- src/main/java/my/examples/studymanager/service/StudyContentService.java
+++ src/main/java/my/examples/studymanager/service/StudyContentService.java
@@ -1,15 +1,10 @@
 package my.examples.studymanager.service;
 
-import my.examples.studymanager.domain.Curriculum;
 import my.examples.studymanager.domain.StudyContent;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 
 public interface StudyContentService {
     public StudyContent getStudyContentByCurriculumDetailId(Long curriculumDetailId);
     public void modifyStudyContent(StudyContent studyContent);
     public void addStudyContent(StudyContent studyContent, Long userId);
-
 }
